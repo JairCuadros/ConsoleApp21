e0202f8998faa1e6856641f51c5c3b628f2aa72c
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-09-21 23:46:45 -0500

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ingrese la cantidad de notas a promediar: ");
+int cantidad = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int promedio = 0;
+int suma = 0;
+while (i < cantidad)
+{
+    Console.Write("Ingrese la nota "+(i+1)+" : ");
+    int nota=int.Parse(Console.ReadLine());
+    suma = suma + nota;
+    promedio = nota / cantidad;
+    i++;
+}
+Console.WriteLine("El promedio es: " + promedio);
